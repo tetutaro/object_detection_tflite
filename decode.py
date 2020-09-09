f3aa330d439b9bc2e891f7584108409f74eca852
Tetsutaro Maruyama <tetsutaro.maruyama@gmail.com>
2020-09-09 13:04:41 +0900

    create non-maximum-supression function

--- decode.py
+++ decode.py
@@ -6,7 +6,6 @@ import os
 import re
 import numpy as np
 from PIL import Image
-import cv2
 import tflite_runtime.interpreter as tflite
 import platform
 if platform.system() == 'Linux':  # RaspberryPi
@@ -19,27 +18,60 @@ else:
 EXTRACT_PAREN = re.compile(r'(?<=\().+?(?=\))')
 
 
+# Intersection of Union
+# boxesX is numpy array of
+# offset 0: min x of bounding box
+# offset 1: min y of bounding box
+# offset 2: max x (x + width) of bounding box
+# offset 3: max y (y + height) of bounding box
 def bboxes_iou(boxes1: np.array, boxes2: np.array) -> np.array:
-    boxes1_area = (
-        boxes1[..., 2] - boxes1[..., 0]
-    ) * (
-        boxes1[..., 3] - boxes1[..., 1]
-    )
-    boxes2_area = (
-        boxes2[..., 2] - boxes2[..., 0]
-    ) * (
-        boxes2[..., 3] - boxes2[..., 1]
-    )
-    left_up = np.maximum(boxes1[..., :2], boxes2[..., :2])
-    right_down = np.minimum(boxes1[..., 2:], boxes2[..., 2:])
+    boxes1_area = (boxes1[:, 2] - boxes1[:, 0]) * (boxes1[:, 3] - boxes1[:, 1])
+    boxes2_area = (boxes2[:, 2] - boxes2[:, 0]) * (boxes2[:, 3] - boxes2[:, 1])
+    left_up = np.maximum(boxes1[:, :2], boxes2[:, :2])
+    right_down = np.minimum(boxes1[:, 2:], boxes2[:, 2:])
     intersection = np.maximum(right_down - left_up, 0.0)
-    inter_area = intersection[..., 0] * intersection[..., 1]
+    inter_area = intersection[:, 0] * intersection[:, 1]
     union_area = boxes1_area + boxes2_area - inter_area
     ious = np.maximum(
         1.0 * inter_area / union_area,
         np.finfo(np.float32).eps
     )
     return ious
+
+
+# Non-Maximum Supression
+# https://towardsdatascience.com/non-maximum-suppression-nms-93ce178e177c
+# bboxes is numpy array of
+# offset 0: min x of bounding box
+# offset 1: min y of bounding box
+# offset 2: max x (x + width) of bounding box
+# offset 3: max y (y + height) of bounding box
+# offset 4: class ID (int)
+# offset 5: probability
+def non_maximum_supression(bboxes: np.array) -> List:
+    unique_class_ids = list(set(bboxes[:, 4]))
+    best_bboxes = list()
+    for cls in unique_class_ids:
+        cls_mask = (bboxes[:, 4] == cls)
+        cls_bboxes = bboxes[cls_mask]
+        while len(cls_bboxes) > 0:
+            max_ind = np.argmax(cls_bboxes[:, 5])
+            best_bbox = cls_bboxes[max_ind]
+            best_bboxes.append(best_bbox)
+            cls_bboxes = np.concatenate(
+                [cls_bboxes[:max_ind], cls_bboxes[max_ind + 1:]]
+            )
+            iou = bboxes_iou(
+                np.array(best_bbox[np.newaxis, :4]),
+                np.array(cls_bboxes[:, :4])
+            )
+            weight = np.ones((len(iou),), dtype=np.float32)
+            iou_mask = iou > 0.213
+            weight[iou_mask] = 0
+            cls_bboxes[:, 5] = cls_bboxes[:, 5] * weight
+            score_mask = cls_bboxes[:, 5] > 0
+            cls_bboxes = cls_bboxes[score_mask]
+    return best_bboxes
 
 
 def sigmoid(x: np.array) -> np.array:
@@ -386,31 +418,10 @@ class DecoderYOLO(Decoder):
         boxes = boxes[mask]
         class_ids = class_ids[mask]
         scores = scores[mask]
-        unique_class_ids = list(set(class_ids))
         bboxes = np.concatenate([
             boxes, class_ids[:, np.newaxis], scores[:, np.newaxis]
         ], axis=-1)
-        best_bboxes = list()
-        for cls in unique_class_ids:
-            cls_mask = (bboxes[:, 4] == cls)
-            cls_bboxes = bboxes[cls_mask]
-            while len(cls_bboxes) > 0:
-                max_ind = np.argmax(cls_bboxes[:, 5])
-                best_bbox = cls_bboxes[max_ind]
-                best_bboxes.append(best_bbox)
-                cls_bboxes = np.concatenate(
-                    [cls_bboxes[:max_ind], cls_bboxes[max_ind + 1:]]
-                )
-                iou = bboxes_iou(
-                    np.array(best_bbox[np.newaxis, :4]),
-                    np.array(cls_bboxes[:, :4])
-                )
-                weight = np.ones((len(iou),), dtype=np.float32)
-                iou_mask = iou > 0.213
-                weight[iou_mask] = 0
-                cls_bboxes[:, 5] = cls_bboxes[:, 5] * weight
-                score_mask = cls_bboxes[:, 5] > 0
-                cls_bboxes = cls_bboxes[score_mask]
+        best_bboxes = non_maximum_supression(bboxes=bboxes)
         bboxes = list()
         for bbox in best_bboxes:
             cid = int(bbox[4])
@@ -556,10 +567,17 @@ class PredictorMobileNet(Predictor):
     def predict(self: PredictorMobileNet, image: Image, bboxes: List) -> List:
         return self._predict(image=image, bboxes=bboxes, imagesize=224)
 
-    def _predict(self: PredictorMobileNet, image: Image, bboxes: List, imagesize: int) -> List:
+    def _predict(
+        self: PredictorMobileNet,
+        image: Image,
+        bboxes: List,
+        imagesize: int
+    ) -> List:
         objects = list()
         for bbox in bboxes:
-            background = Image.new("RGB", (imagesize, imagesize), (128, 128, 128))
+            background = Image.new(
+                "RGB", (imagesize, imagesize), (128, 128, 128)
+            )
             x, y, w, h = bbox
             cropped = image.crop((x, y, x+w, y+h))
             if w >= h:
@@ -589,6 +607,7 @@ class PredictorMobileNet(Predictor):
             if label != 'background' and prob >= self.threshold:
                 objects.append({
                     'name': label,
+                    'index': idx,
                     'prob': prob,
                     'bbox': (x, y, x + w, y + h),
                 })
