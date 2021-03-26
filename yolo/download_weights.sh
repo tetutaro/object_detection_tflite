f5d4b42ba064518ee0abdc56818a4f01c2a42992
Tetsutaro Maruyama <tetsutaro.maruyama@gmail.com>
2021-03-27 00:13:42 +0900

    download yolov4.weights from github

--- yolo/download_weights.sh
+++ yolo/download_weights.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 wget https://pjreddie.com/media/files/yolov3.weights
 wget https://pjreddie.com/media/files/yolov3-tiny.weights
-wget -O yolov4.weights "https://drive.google.com/open?id=1cewMfusmPjYWbrnuJRuKhPMwRe_b9PaT"
+wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights
